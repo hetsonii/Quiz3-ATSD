0bfcce9fd5a4c6aa70e465a6d55e9ab99d77c938
hetsonii <21cs068@charusat.edu.in>
2025-10-28 15:36:58 -0300

    Added AssignTo Method in Task class

---
+++ Task.java
@@ -0,0 +1,22 @@
+
+import java.util.*;
+import java.io.*;
+
+public class Task {
+    private List<String> assignedMembers = new ArrayList<>();
+
+    public void assignTo(String member) {
+        assignedMembers.add(member);
+    }
+
+    public List<String> getAssignedMembers() {
+        return assignedMembers;
+    }
+
+    public static void main(String[] args) {
+        Task task = new Task();
+        task.assignTo("Alice");
+        task.assignTo("Bob");
+        System.err.println("Assigned Members: " + task.getAssignedMembers());
+    }
+}
